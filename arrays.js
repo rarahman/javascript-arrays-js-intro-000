d34324d61d848118406284e8f51d661bdb3a49b8
Raiyan Rahman <raiyan.rahman4@gmail.com>
2018-12-30 17:49:24 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,8 +1,10 @@
 var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles']
 
 function addElementToBeginningOfArray(array, arg) {
-  var newArray = new array
-  return array.unshift(arg);
+  var newArray = new Array();
+  //return array.unshift(arg);
+  newArray = [arg, ...array]
+  return newArray
 }
 
 function destructivelyAddElementToBeginningOfArray(array, arg) {
