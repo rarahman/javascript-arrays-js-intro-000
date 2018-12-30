3029bb4b90cfe6ffc6c4e88b54cd7889dc8e600c
Raiyan Rahman <raiyan.rahman4@gmail.com>
2018-12-30 22:23:37 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,25 +1,41 @@
 var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles']
 
-function addElementToBeginningOfArray(array, arg) {
-  var newArray = new Array();
-  //return array.unshift(arg);
-  newArray = [arg, ...array]
-  return newArray
+function addElementToBeginningOfArray(chocolateBars, candyString){
+  return [candyString, ...chocolateBars];
 }
-
-function destructivelyAddElementToBeginningOfArray(array, arg) {
-  array.unshift(arg);
-  return array
+ 
+function destructivelyAddElementToBeginningOfArray(chocolateBars, candyString){
+  chocolateBars.unshift(candyString);
+  return chocolateBars;
 }
-
-function addElementToBeginningOfArray(array, element) {
-  array.push(element)
-  return array
+ 
+function addElementToEndOfArray(chocolateBars, candyString){
+  return [...chocolateBars, candyString];
 }
-
-function destructivelyAddElementToEndOfArray(array, element) {
-  var nArray = new Array();
-  nArray = [...array, element]
-  return nArray
+ 
+function destructivelyAddElementToEndOfArray(chocolateBars, candyString){
+  chocolateBars.push(candyString);
+  return chocolateBars;
 }
-
+ 
+function accessElementInArray(array, index){
+  return array[index];
+}
+ 
+function destructivelyRemoveElementFromBeginningOfArray(chocolateBars){
+  chocolateBars.shift();
+  return chocolateBars;
+}
+ 
+function removeElementFromBeginningOfArray(chocolateBars){
+  return chocolateBars.slice(1);
+}
+ 
+function destructivelyRemoveElementFromEndOfArray(chocolateBars){
+  chocolateBars.pop();
+  return chocolateBars;
+}
+ 
+function removeElementFromEndOfArray(chocolateBars){
+  return chocolateBars.slice(0, chocolateBars.length - 1);
+}
