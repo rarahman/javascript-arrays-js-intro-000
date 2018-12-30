37d892ddcf71cbecd389d51bc49d772a55d408d7
Raiyan Rahman <raiyan.rahman4@gmail.com>
2018-12-30 17:53:35 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -9,7 +9,7 @@ function addElementToBeginningOfArray(array, arg) {
 
 function destructivelyAddElementToBeginningOfArray(array, arg) {
   //return array.unshift(arg);
-  array = [arg, ...array]
+  array = array.unshift(arg)
   return array
 }
 
